6dc1efe09e19cf7aa479b245deba4bc261952579
asih <hamzassi.91@gmail.com>
2020-04-30 12:07:20 +0300

    add db for the app + db migration support

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from src.app import app
+from src.app import app, db
+from src.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "Post": Post, "User": User}
 
 
 if __name__ == "__main__":
--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard-to-guess-secret-key"
+    default_db_url = f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or default_db_url
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
