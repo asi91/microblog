eb5dc9b1e34aec339dbd85c713e2a278a0bc522d
asih <hamzassi.91@gmail.com>
2020-04-30 14:18:47 +0300

    Support user login, logout and registration

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from .models import User
 
 
 class UserLogin(FlaskForm):
@@ -9,3 +10,20 @@ class UserLogin(FlaskForm):
     remember_me = BooleanField("remember me")
     submit = SubmitField("submit")
 
+
+class Registration(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    password2 = PasswordField("repeat password", validators=[DataRequired(), EqualTo("password")])
+    email = StringField("email", validators=[DataRequired(), Email()])
+    submit = SubmitField("register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Please use a different user name")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(username=email.data).first()
+        if user:
+            raise ValidationError("Please use a different email")
--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,15 @@
-from .app import app
-from flask import render_template, redirect, flash, url_for
-from .forms import UserLogin
+from .app import app, db
+from flask import render_template, redirect, flash, url_for, request
+from .forms import UserLogin, Registration
+from flask_login import current_user, login_user, logout_user, login_required
+from .models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    users = {"username": "Hamza"}
     posts = [
         {
             "author": {"username": "John Legend"},
@@ -17,13 +20,48 @@ def index():
             "body": "I am Python Developer"
         }
     ]
-    return render_template("index.html", title="Humble Blog Homepage", user=users, posts=posts)
+    return render_template("index.html", title="Humble Blog Homepage", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = Registration()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for("login"))
+
+    return render_template("register.html", form=form, title="Register")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = UserLogin()
     if form.validate_on_submit():
-        flash(f"Login requested for user `{form.username.data}`, remember me={form.remember_me.data}")
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash(f"Invalid user name or password, please try again")
+            return redirect(url_for("register"))
+        login_user(user, remember=form.remember_me.data)
+        redirect_page = request.args.get("next")
+        if not redirect_page or url_parse(redirect_page).netloc != "":
+            redirect_page = url_for("index")
+
+        return redirect(redirect_page)
+
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
